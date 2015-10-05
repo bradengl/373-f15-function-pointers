46214325e3e01ea2b5f3d9ebc0cd29b143b61f9c
bradengl <bradengl@umich.edu>
2015-10-04 21:22:00 -0400

    Update reverse_sort.c

--- reverse_sort.c
+++ reverse_sort.c
@@ -1,1 +1,11 @@
+#include <string.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
+#include "sort.h"
+
+//__attribute__ ((weak))
+int compare(int a, int b) {
+	    return b - a;
+}
